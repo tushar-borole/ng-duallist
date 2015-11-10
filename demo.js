ce0e1aa43ec669ed6b0a2c1d4f58553d6fb861cc
Tushar Borole <tushariscoolster@gmail.com>
2015-11-10 19:08:17 +0530

    Duallist chages done

--- demo.js
+++ demo.js
@@ -1,28 +1,50 @@
-(function() {
+(function () {
     'use strict';
 
     angular
-        .module('demoApp',['ng-duallist'])
+        .module('demoApp', ['ng-duallist'])
         .controller('demoController', demoController);
 
     demoController.$inject = ['$rootScope'];
 
     /* @ngInject */
-    function demoController($rootScope){
+    function demoController($rootScope) {
         var vm = this;
         vm.property = 'demoController';
-        
+
 
         activate();
 
         ////////////////
 
         function activate() {
-  
-            
-            vm.leftValue=[{'name':'satish1'},{'name':'manish1'}]
-             vm.rightValue=[{'name':'satish2'},{'name':'manish2'}]
-          
+
+
+            vm.leftValue = [{
+                'name': 'left1'
+            }, {
+                'name': 'left2'
+            }, {
+                'name': 'left3'
+            }, {
+                'name': 'left4'
+            }, {
+                'name': 'left5'
+            }, {
+                'name': 'left6'
+            }, {
+                'name': 'left7'
+            }, {
+                'name': 'left8'
+            }, {
+                'name': 'left9'
+            }]
+            vm.rightValue = [{
+                'name': 'satish2'
+            }, {
+                'name': 'manish2'
+            }]
+
         }
     }
 })();
--- ngduallist.js
+++ ngduallist.js
@@ -22,6 +22,9 @@
                 leftscope: '=',
                 rightscope: '='
             },
+            bindToController: false,
+            controller: Controller,
+            controllerAs: 'vm',
             templateUrl: function (element, attrs) {
                 return attrs.templateUrl || 'dual/duallist.html';
             },
@@ -31,18 +34,48 @@
 
         function link(scope, element, attrs, controller) {
 
+
+            $(element).find('.left-container').bind('scroll', function () {
+                if ($(this)[0].scrollHeight - $(this).scrollTop() === $(this).outerHeight()) {
+
+                };
+            });
+
+
+            $(element).find('.right-container').bind('scroll', function () {
+                if ($(this)[0].scrollHeight - $(this).scrollTop() === $(this).outerHeight()) {
+
+                };
+            });
+
+
+
+        }
+
+    }
+    
+       Controller.$inject = ['$scope'];
+
+    /* @ngInject */
+    function Controller($scope) {
+        
+        
+
+
+
             /**
              * @description move the selected item to the right
              */
-            scope.moveRight = function () {
+            $scope.moveRight = function () {
+             
                 //var leftSelectedValue=$filter()
-                angular.forEach(scope.leftscope, function (val) { // push the value to right array
+                angular.forEach($scope.leftscope, function (val) { // push the value to right array
                     if (val.selected) {
                         val.selected = false
-                        scope.rightscope.push(val);
+                        $scope.rightscope.push(val);
                         //delete val;
-                        var index = scope.leftscope.indexOf(val)
-                        scope.leftscope.splice(index, 1)
+                        var index = $scope.leftscope.indexOf(val)
+                        $scope.leftscope.splice(index, 1)
                     }
 
                 });
@@ -52,14 +85,14 @@
             /**
              * @description move the selected item to the left
              */
-            scope.moveLeft = function () {
+            $scope.moveLeft = function () {
                 //var leftSelectedValue=$filter()
-                angular.forEach(scope.rightscope, function (val) { // push the value to right array
+                angular.forEach($scope.rightscope, function (val) { // push the value to right array
                     if (val.selected) {
                         val.selected = false
-                        scope.leftscope.push(val);
-                        var index = scope.rightscope.indexOf(val)
-                        scope.rightscope.splice(index, 1)
+                        $scope.leftscope.push(val);
+                        var index = $scope.rightscope.indexOf(val)
+                        $scope.rightscope.splice(index, 1)
                             //delete val;
                     }
 
@@ -68,10 +101,13 @@
 
 
 
-            scope.selectAllLeftContainer = function () {
+            /**
+             * @description select all left container
+             */
+            $scope.selectAllLeftContainer = function () {
 
-                angular.forEach(scope.leftscope, function (val) {
-                    if (scope.leftSelectAll) {
+                angular.forEach($scope.leftscope, function (val) {
+                    if ($scope.leftSelectAll) {
                         val.selected = true;
                     } else {
                         val.selected = false;
@@ -79,9 +115,12 @@
                 })
             }
 
-            scope.selectAllRightContainer = function () {
-                angular.forEach(scope.rightscope, function (val) {
-                    if (scope.rightSelectAll) {
+            /**
+             * @description select all right container
+             */
+            $scope.selectAllRightContainer = function () {
+                angular.forEach($scope.rightscope, function (val) {
+                    if ($scope.rightSelectAll) {
                         val.selected = true;
                     } else {
                         val.selected = false;
@@ -89,13 +128,11 @@
                 })
 
             }
-
-        }
-
+        
     }
 
     function templateCache($templateCache) {
-        $templateCache.put('dual/duallist.html', '<div class="row"> <div class="col-md-12 text-center"></div><div class="col-sm-4 col-sm-offset-1"> <div class="list-group" id="list1"> <a href="#" class="list-group-item active">List 1 <input title="toggle all" class="all pull-right" ng-model="leftSelectAll" ng-change="selectAllLeftContainer()" type="checkbox"></a> <a ng-repeat="data in leftscope" href="#" class="list-group-item">{{data.name}}<input class="pull-right" ng-model="data.selected" type="checkbox"></a>  </div></div><div class="col-md-2 v-center"> <button ng-click="moveRight()" title="Send to list 2" class="btn btn-default center-block add"><i class="glyphicon glyphicon-chevron-right"></i></button> <button ng-click="moveLeft()" title="Send to list 1" class="btn btn-default center-block remove"><i class="glyphicon glyphicon-chevron-left"></i></button> </div><div class="col-sm-4"> <div class="list-group" id="list2"> <a href="#" class="list-group-item active">List 2 <input ng-model="rightSelectAll" ng-change="selectAllRightContainer()" title="toggle all" class="all pull-right" type="checkbox"></a> <a ng-repeat="data in rightscope" href="#" class="list-group-item">{{data.name}} <input ng-model="data.selected"  class="pull-right" type="checkbox"></a>  </div></div></div>');
+        $templateCache.put('dual/duallist.html', '<div class="row ngduallist"><div class="col-sm-4 left-container"> <div class="list-group" id="list1"> <a href="#" class="list-group-item active">List 1 <input title="toggle all" class="all pull-right" ng-model="leftSelectAll" ng-change="selectAllLeftContainer()" type="checkbox"></a> <a ng-repeat="data in leftscope" href="#" class="list-group-item">{{data.name}}<input class="pull-right" ng-model="data.selected" type="checkbox"></a>  </div></div><div class="col-md-2 v-center"> <button ng-click="moveRight()"  title="Send to list 2" class="btn btn-default center-block add"><i class="glyphicon glyphicon-chevron-right"></i></button> <button ng-click="moveLeft()" title="Send to list 1" class="btn btn-default center-block remove"><i class="glyphicon glyphicon-chevron-left"></i></button> </div><div class="col-sm-4 right-container"> <div class="list-group" id="list2"> <a href="#" class="list-group-item active">List 2 <input ng-model="rightSelectAll" ng-change="selectAllRightContainer()" title="toggle all" class="all pull-right" type="checkbox"></a> <a ng-repeat="data in rightscope" href="#" class="list-group-item">{{data.name}} <input ng-model="data.selected"  class="pull-right" type="checkbox"></a>  </div></div></div>');
 
     }
 
